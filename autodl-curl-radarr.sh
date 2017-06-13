7aca817f79247850650d59332a77e2b0c5b720bf
alaq <alaq@users.noreply.github.com>
2017-06-13 10:16:04 -0400

    Create autodl-curl-radarr.sh

---
+++ autodl-curl-radarr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+title=$1
+downloadUrl=$2
+apiKey=$3
+date=$(date -u +"%Y-%m-%d %H:%M:%SZ")
+
+{
+/usr/bin/curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "X-Api-Key: $apiKey" -X POST -d '{"title":"'"$title"'","downloadUrl":"'"$downloadUrl"'","downloadProtocol":"torrent","publishDate":"'"$date"'"}' http://localhost:40005/api/release/push
+} &> /dev/null
